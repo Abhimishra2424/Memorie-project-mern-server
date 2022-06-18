e4b67d872e9f4143c5717ced471eda6eaab4eed2
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 16:09:17 +0530

    base setup controllers

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,10 +1,8 @@
 import express from "express";
+import { getPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
-// http://localhost:5000/posts
-router.get("/", (req, res) => {
-  res.send("This is Post");
-});
+router.get("/", getPosts);
 
 export default router;
