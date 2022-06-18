c796d1695d2e82f2b0dca0a8228d76fec6879059
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 16:26:58 +0530

    aaded function to get post create post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,27 @@
-export const getPosts = (req, res) => {
-  res.send("This is Post");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await PostMessage.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
 };
