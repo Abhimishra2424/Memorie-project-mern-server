aa4e83e0ac52728eb7ab95aec81fa3524da97021
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 16:14:37 +0530

    create Post Model

---
+++ models/postMessage.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title: { type: String },
+  message: { type: String },
+  creator: { type: String },
+  tags: [String],
+  selectedFile: { type: String },
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: { type: Date, default: new Date() },
+});
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
