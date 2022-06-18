9b363a4cd7b1e2447a93cbc747f554570770846b
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 23:41:53 +0530

    fixed cors problem

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import postRoutes from "./routes/Posts.js";
 
 const app = express();
 
-// http://localhost:5000/posts
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
+
+// http://localhost:5000/posts
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://abhi:abhi24@cluster0.jehixcx.mongodb.net/?retryWrites=true&w=majority";
