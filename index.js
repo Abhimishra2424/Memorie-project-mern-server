b45c435885c973669dd84cc7db964e163deac31f
Abhishek Mishra <abmishra700@gmail.com>
2022-06-25 09:38:47 +0530

    aaded env

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from 'dotenv'
 import postRoutes from "./routes/Posts.js";
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,12 +17,10 @@ app.use(morgan("dev"));
 // http://localhost:5000/posts
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://abhi:abhi24@cluster0.jehixcx.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
