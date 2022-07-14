9f7492a567ade6dbbcfb3417c3fe99dbe35221d5
Abhishek Mishra <abmishra700@gmail.com>
2022-07-14 13:58:15 +0530

    modify getPosts api for  pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,9 +2,20 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const posts = await PostMessage.find();
-    res.status(200).json(posts);
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({
       message: error.message,
